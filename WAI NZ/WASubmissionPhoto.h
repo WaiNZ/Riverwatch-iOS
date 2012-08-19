0e7b7e6418b6fedbfccc0f71510687f548593124
Melby Ruarus <melby.ruarus@gmail.com>
2012-08-19 17:13:55 +1200

    Commented WASubmissionPhoto

--- WAI NZ/WASubmissionPhoto.h
+++ WAI NZ/WASubmissionPhoto.h
@@ -9,6 +9,10 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
+/**
+ * A photo data object that encapsulates the image data, timestamp,
+ * and location if available for a submission
+ */
 @interface WASubmissionPhoto : NSObject 
 {
     UIImage *image;
@@ -16,13 +20,49 @@
     CLLocation *location;
 }
 
+/**
+ * A convenience method for creating a WASubmissionPhoto from
+ * the result data of a UIImagePickerController.
+ *
+ * This method is asynchronous and returns immediately due to use of
+ * AssetsLibrary for accessing location/timestamp information of photos
+ * selected from the devices camera roll.
+ *
+ * If the photo selected is located on the camera roll then
+ * on iOS 6 and above the user will be prompted to allow the
+ * application access to the camera roll. On iOS 5 and below
+ * the user will be prompted to allow the application access to
+ * location services. If the user denys these or location services
+ * is not enabled on iOS 5 and below this method will fail.
+ *
+ * **NOTE:** Only images are supported.
+ *
+ * **NOTE:** iOS 6: requires access to photo library for photos from camera roll
+ *
+ * **NOTE:** iOS 5 and below: for photos from camera roll requires access
+ * to location services, and location services must be enabled
+ *
+ * @param dict the dictionary returned by a UIImagePickerController
+ * @param resultBlock the block called whith the WASubmissionPhoto when successfull
+ * @param failureBlock called when a failure occurs
+ */
 + (void)photoWithMediaPickingInfo:(NSDictionary *)dict
 					  resultBlock:(void (^)(WASubmissionPhoto *photo))resultBlock
 					 failureBlock:(void (^)(NSError *error))failureBlock;
 
+/**
+ * Initilize a WASubmissionPhoto with the specified parameters
+ * 
+ * @param image the image
+ * @param timestamp the UNIX time at which the photo was taken
+ * @param location the location the the photo was taken at: optional.
+ */
 - (id)initWithPhoto:(UIImage *)image timestamp:(time_t)timestamp location:(CLLocation *)location;
 
+/** The location this photo was taken at, may be nil */
 @property (atomic, readonly) CLLocation *location;
+/** The UNIX time the photo was taken at */
 @property (atomic, readonly) time_t timestamp;
+/** The image data for this photo */
 @property (atomic, readonly) UIImage *image;
 @end
--- WAI NZ/WASubmissionPhoto.m
+++ WAI NZ/WASubmissionPhoto.m
@@ -25,11 +25,15 @@
 
 + (void)photoWithMediaPickingInfo:(NSDictionary *)dict
 					  resultBlock:(void (^)(WASubmissionPhoto *photo))resultBlock
-					 failureBlock:(void (^)(NSError *error))failureBlock {	
+					 failureBlock:(void (^)(NSError *error))failureBlock {
+	// Extract the image object
 	UIImage *_image = [dict objectForKey:UIImagePickerControllerOriginalImage];
+	// Declare variables to hold date/location information
+	// These are prefixed with __block so that they can be modified asynchronously by the callbacks from ALAssetsLibrary
 	__block NSDate *date = nil;
 	__block CLLocation *location = nil;
 	
+	// Delcare a helper block to avoide having to type this code multiple times
 	void (^success)(void) = ^{
 		if(resultBlock) {
 			resultBlock([[WASubmissionPhoto alloc] initWithPhoto:_image timestamp:[date UNIXTimestamp] location:location]);
@@ -38,39 +42,65 @@
 	
 	NSDictionary *metadata;
 	if((metadata = [dict objectForKey:@"UIImagePickerControllerMediaMetadata"])) { // TODO: check 4.0 support
+		// If the dictionary contains metadata info - i.e. user took a photo
+		
+		// Get the date from the Exif dictionary
 		NSDictionary *exif = [metadata objectForKey:@"{Exif}"];
 		date = [exif objectForKey:@"DateTimeOriginal"];
 		
 		if(!date) {
+			// If the date wasn't in the Exif
+			
+			// Get the date from the TIFF dictionary
 			NSDictionary *tiff = [metadata objectForKey:@"{TIFF}"];
 			date = [tiff objectForKey:@"DateTime"];
+			
+			// TODO: no date!!! error
 		}
 		
+		// All good
 		success();
 	}
 	else {
+		// If this isn't a photo with metadata - i.e. selected from camera roll
+		
 		NSURL *assetsURL;
 		if((assetsURL = [dict objectForKey:@"UIImagePickerControllerReferenceURL"])) { // TODO: check 4.0 support
+			// If it has an assets url that we can look up metadata with
+			
+			// Get a link to the assets library
 			ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+			// Load this asset
 			[library assetForURL:assetsURL
 					 resultBlock:^(ALAsset *asset) {
+						 // Asset was loaded
+						 
+						 // Get the date
 						 id tmpDate = [asset valueForProperty:ALAssetPropertyDate];
 						 if(tmpDate != ALErrorInvalidProperty) {
 							 date = tmpDate;
 						 }
+						 else {
+							 // TODO: Error??
+						 }
 						 
+						 // Get the optional location
 						 id tmpLocation = [asset valueForProperty:ALAssetPropertyLocation];
 						 if(tmpLocation != ALErrorInvalidProperty) {
 							 location = tmpLocation;
 						 }
 						 
+						 // All good
 						 success();
 					 }
 					failureBlock:^(NSError *error) {
+						// Error looking up the asset
 						// TODO: error!!!!!
 					}];
 		}
 		else {
+			// Unrecognized image selection!
+			
 			// TODO: error!!!!!
 		}
 	}
